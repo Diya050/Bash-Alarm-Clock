3bef54cbb75a4b3841306896a4f72e28c0a424b5
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-04-04 19:21:16 +0530

    Update alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -7,10 +7,12 @@ alarm_time="12:00"
 current_time=$(date +"%H:%M")
 
 # Enter a loop to check if the alarm time has been reached
-while [ "$current_time" != "$alarm_time" ]; do
-    sleep 60 # wait for 1 minute
-    current_time=$(date +"%H:%M") # update the current time
+while true; do
+    current_time=$(date +"%H:%M")
+    if [ "$current_time" \> "$alarm_time" ]; then
+        break
+    fi
+    sleep 60
 done
-
 # Play the music file when the alarm goes off
 play file.mp3
